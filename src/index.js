fdc0df23aa57f3dd956bae952dbc4109e7003edb
AdrianMAD <46680955+AdrianMAD@users.noreply.github.com>
2019-02-11 22:01:11 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,20 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
 
-
-function tick() {
-  const element = (
-    <div className="tc ph4">
-    	<div>
-    		<img alt="cats party" src="https://media.giphy.com/media/5wWf7H5pp15X4yOr2ik/giphy.gif" />
-    	</div>
-    		<h1>It is {new Date().toLocaleTimeString()}.</h1>
-    </div>
-  );
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
+const imagePath = {
+  true:"https://media.giphy.com/media/5wWf7H5pp15X4yOr2ik/giphy.gif",
+  false:"https://media1.tenor.com/images/cbad6bc097e72d9a04628cbecc576265/tenor.gif?itemid=5368434"
 }
 
-setInterval(tick, 1000);
+class Clock extends React.Component{
+  constructor (props) {
+    super (props);
+    this.state={image: true}
+  }
+  changeImage = () => {
+      this.setState(state =>({image: !state.image}))
+    }
+  getImageName = () =>this.state.image ? true : false;
+
+  render () {
+    const imageName = this.getImageName();
+    return (
+      <div className="tc ph4">
+        <div>
+          <img alt="cats party" src={imagePath[imageName]} />
+        </div>
+          <h1>It is {new Date().toLocaleTimeString()}.</h1>
+          <button type="button" onClick={this.changeImage}>
+          It's cat-o-clock time
+          </button>
+    </div>
+      )
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById('root'))
+
+ 
+
+// setInterval(Clock, 1000);
+
