df0f99b37bfce641a98a22f47efd7271f359a57e
AdrianMAD <46680955+AdrianMAD@users.noreply.github.com>
2019-02-13 21:57:19 +0200

    additional feature

--- src/index.js
+++ src/index.js
@@ -8,35 +8,63 @@ const imagePath = {
   false:"https://media1.tenor.com/images/cbad6bc097e72d9a04628cbecc576265/tenor.gif?itemid=5368434"
 }
 
+const buttonText = {
+  true: "It's cat-o-clock time",
+  false: "Dammit, the cat party is over"
+}
+
 class Clock extends React.Component{
   constructor (props) {
     super (props);
-    this.state={image: true}
+    this.state={
+      image: true,
+      text: true,
+    }
   }
+
+  time = () => {
+    return new Date().toLocaleTimeString();
+  }
+  
   changeImage = () => {
       this.setState(state =>({image: !state.image}))
     }
+  changeText = () => {
+    this.setState(state =>({text : !state.text}))
+  }
   getImageName = () =>this.state.image ? true : false;
+
+  getTextTag = () => this.state.text ? true : false;
+
+  handleClick = () =>{
+    this.changeImage();
+    this.changeText();
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
   render () {
     const imageName = this.getImageName();
+    const textName = this.getTextTag();
     return (
       <div className="tc ph4 vh-100 dt w-100">
         <div>
           <img alt="cats party" className="dtc" src={imagePath[imageName]} />
         </div>
-          <h1>It is {new Date().toLocaleTimeString()}.</h1>
-          <button type="button" onClick={this.changeImage}>
-          It's cat-o-clock time
+          <h1>It is {`${this.time()}`}.</h1>
+          <button type="button" onClick={this.handleClick}>
+          {buttonText[textName]}
           </button>
-    </div>
+      </div>
       )
   }
 }
 
 ReactDOM.render(<Clock />, document.getElementById('root'))
 
- 
-
-// setInterval(<Clock />, 1000);
 
